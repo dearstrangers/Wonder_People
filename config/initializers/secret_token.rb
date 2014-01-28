47e490032111da2dff80de8984091cc72f306687
Ng Hon Keung <Eug@Ng-Hon-Keungs-MacBook-Pro.local>
2014-01-28 18:22:47 +0800

    new project added gem rspec factory girl devise and rails g devise cleaner and admin. also rake db:migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WonderPeople::Application.config.secret_key_base = '2079ef001919380b35660365bb0aab815e7eec9b9cdd45047e7071e0f33f8ab6c201b4840ecffadab87ac0849105b92df2e9423f5cfdc35cdf8ff8414aa9f578'
